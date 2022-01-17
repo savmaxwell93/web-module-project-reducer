1bf117445de23c41a58e958127a744c4ec991b7f
Savannah <savmaxwell@gmail.com>
2022-01-17 12:28:22 -0700

    connected applyNumber

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,17 @@ import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
 import reducer, { initialState } from '../reducers';
-import { addOne } from '../actions';
+import { addOne, applyNumber } from '../actions';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   const handleAdd = () => {
     dispatch(addOne())
+  }
+
+  const handleClick = (number) => {
+    dispatch(applyNumber(number))
   }
 
   return (
@@ -38,21 +42,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton onClick={handleAdd} value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick={() => {handleClick(1)}} value={1}/>
+              <CalcButton onClick={() => {handleClick(2)}} value={2}/>
+              <CalcButton onClick={() => {handleClick(3)}} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick={() => {handleClick(4)}} value={4}/>
+              <CalcButton onClick={() => {handleClick(5)}} value={5}/>
+              <CalcButton onClick={() => {handleClick(6)}} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick={() => {handleClick(7)}} value={7}/>
+              <CalcButton onClick={() => {handleClick(8)}} value={8}/>
+              <CalcButton onClick={() => {handleClick(9)}} value={9}/>
             </div>
 
             <div className="row">
